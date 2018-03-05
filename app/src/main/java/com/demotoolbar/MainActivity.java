fc4725b2ffe0398fc3f8fc789ae3bdd199f2f586
perry <perry091061@gmail.com>
2018-03-05 15:26:17 +0000

    Basic toolbar replacement

---
+++ app/src/main/java/com/demotoolbar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.demotoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            toolbar.setTitle("A Toolbar");
+            toolbar.setSubtitle("with a subtitle");
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+}
